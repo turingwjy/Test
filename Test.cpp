f8098803abfa94c68ca69b8b4bb5f39728daf103
root <root@ubuntu.(none)>
2017-03-14 11:51:28 +0800

    test

---
+++ Test.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Test.cpp
+ *
+ *    Description:  wjy
+ *
+ *        Version:  1.0
+ *        Created:  2017年03月14日 11时42分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int main( int argc, char**argv )
+{
+    return 0;
+}
